4a93bc37d24b38c56a1e25ffbe3f476008873d3f
bwgjoseph <josephgan@live.com.sg>
2022-09-24 19:41:08 +0800

    add spotless configured with palantir-java-format
    include vscode settings with spotless plugin https://marketplace.visualstudio.com/items?itemName=richardwillis.vscode-spotless-gradle
    
    ```
    "[java]": {
    	"editor.codeActionsOnSave": {
    		"source.organizeImports": true,
    		"source.fixAll.spotlessGradle": true
    	},
    	"spotlessGradle.format.enable": true,
    	"spotlessGradle.diagnostics.enable": true
    }
    ```

--- src/main/java/com/bwgjoseph/springdatamongoeverything/SpringDataMongoEverythingApplication.java
+++ src/main/java/com/bwgjoseph/springdatamongoeverything/SpringDataMongoEverythingApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringDataMongoEverythingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDataMongoEverythingApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataMongoEverythingApplication.class, args);
+    }
 }
